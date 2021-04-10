53d95c17b0fbdddaef6d07da46e02a90c8ce834b
borgnine <derlrobbins@gmail.com>
2021-04-10 15:05:41 -0400

    adds passport config and user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,11 @@
 const express = require( 'express' )
-const router = express.router()
+const router = express.Router()
 const bcrypt = require( 'bcryptjs' )
 const jwt = require( 'jsonwebtoken' )
 const validateRegisterInput = require( '../../validation/register' )
 const validateLoginInput = require( '../../validation/login')
 const User = require( '../../models/User')
-
+require('dotenv').config()
 
 // @route POST api/users/register
 // @desc Register user
@@ -46,4 +46,60 @@ router.post( "/register", ( req, res ) => {
       }
 
     })
+})
+
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+
+  const { errors, isValid } = validateLoginInput( req.body )
+
+  if ( !isValid ) {
+    return res.status(400).json( errors )
+  }
+
+  const email = req.body.email
+  const password = req.body.password
+
+  User.findOne({ email }).then( user => {
+ 
+    if ( !user ) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    bcrypt.compare( password, user.password ).then( isMatch => {
+      if ( isMatch ) {
+  
+        const payload = {
+          id: user.id,
+          name: user.name
+        }
+
+        jwt.sign(
+          payload,
+          process.env.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          ( err, token ) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        )
+      } 
+      else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+
+    })
+
   })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-
+const passport = require('passport')
 const app = express()
+const users = require( './routes/api/users')
 
 //bodyparser 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(passport.initialize())
+require('./config/passport')
+app.use('/api/users', users)
 
 const db = process.env.DB
 
