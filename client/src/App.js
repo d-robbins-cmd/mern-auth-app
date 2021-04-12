110b420d090194103a05a73aade7c757ac88d97e
borgnine <derlrobbins@gmail.com>
2021-04-12 15:04:00 -0400

    more front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layout/navbar'
 import Landing from './components/layout/landing'
+import Login from './components/layout/auth/Login'
+import Register from './components/layout/auth/Register'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ Landing } />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/login" component={ Login } />
+        </div>
+      </Router>
+    );
+  }
+
 }
-
-export default App;
+export default App
--- client/src/components/layout/landing.js
+++ client/src/components/layout/landing.js
@@ -2,7 +2,9 @@ import react, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Landing extends Component {
+
     render() {
+
       return (
         <div style={{ height: "75vh" }} className="container valign-wrapper">
           <div className="row">
@@ -46,7 +48,8 @@ class Landing extends Component {
             </div>
           </div>
         </div>
-      );
+      )
+      
     }
   }
 
